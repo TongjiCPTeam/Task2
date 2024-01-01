3e4529fd6cf3bebf738ce336986c9d46eb9ea0bb
Gravity <12385512+gravity98@user.noreply.gitee.com>
2024-01-01 23:16:22 +0800

    更新一个不报红的版本

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
-import PL0.PL0BaseVisitor;
-import PL0.PL0VisitorImpl;
+import ANTLR.PL0.PL0Lexer;
+import ANTLR.PL0.PL0Parser;
+import ANTLR.PL0.PL0BaseVisitor;
+import ANTLR.PL0.PL0VisitorImpl;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 
@@ -15,11 +17,11 @@ public class Main {
 //        visitor.visit(tree);
 //        System.out.println("parser has executed");
 
-        PL0.PL0Lexer lexer = new PL0.PL0Lexer(CharStreams.fromString("PROGRAM test CONST a = 1; VAR b,c; BEGIN b:=b+a; END"));
-        PL0.PL0Parser parser = new PL0.PL0Parser((new CommonTokenStream(lexer)));
+        PL0Lexer lexer = new PL0Lexer(CharStreams.fromString("PROGRAM test CONST a = 1; VAR b,c; BEGIN b:=b+a; END"));
+        PL0Parser parser = new PL0Parser((new CommonTokenStream(lexer)));
 //        parser.start();
         parser.setBuildParseTree(true);
-        PL0.PL0Parser.StartContext tree = parser.start();
+        PL0Parser.StartContext tree = parser.start();
         PL0BaseVisitor<String> visitor = new PL0VisitorImpl();
         visitor.visit(tree);
 
